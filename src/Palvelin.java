c31bfec90b58bf06a6de68525f60d4925f5602b5
Lassi <Lassi@86.50.127.61>
2016-01-07 20:52:34 +0200

    Lisäilty tarvittavia luokkia ja vähän koodin alkua palvelimeen.

--- src/Palvelin.java
+++ src/Palvelin.java
@@ -1,4 +1,10 @@
-
+import java.rmi.Naming;
 public class Palvelin {
-
-}
+	public static void main(String[] q){
+		try{
+			
+		}catch (Exception e){
+			System.out.println("Error: " + e);
+		}
+	} // main
+} // class Palvelin
